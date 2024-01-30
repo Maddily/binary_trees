4ea53801f2a004f4443effc67ca95f163b17d50f
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-30 11:31:50 +0200

    Update function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,40 +9,15 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left, height_right;
-
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (0);
-
-	height_left = binary_tree_traverse_n_count_side(tree->left);
-	height_right = binary_tree_traverse_n_count_side(tree->right);
-
-	return ((height_left >= height_right) ? height_left + 1 : height_right + 1);
-}
-
-/**
- * binary_tree_traverse_n_count_side - Traverses a binary tree
- * and counts the edges
- *
- * @tree: A pointer to a node in a binary tree
- *
- * Return: The height of the tree
-*/
-size_t binary_tree_traverse_n_count_side(const binary_tree_t *tree)
-{
-	size_t height = 0;
+	size_t height_left = 0, height_right = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left)
-	{
-		height += 1 + binary_tree_traverse_n_count_side(tree->left);
-	}
+		height_left = 1 + binary_tree_height(tree->left);
 	if (tree->right)
-	{
-		height += 1 + binary_tree_traverse_n_count_side(tree->right);
-	}
+		height_right = 1 + binary_tree_height(tree->right);
 
-	return (height);
+	return (MAX(height_left, height_right));
 }
