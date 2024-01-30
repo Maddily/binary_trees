aaf581b71a9f2a5e686f09999ccee1eadec05eaa
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-30 11:02:07 +0200

    Remove function prototype

--- binary_trees.h
+++ binary_trees.h
@@ -71,8 +71,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 
 
-int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif
