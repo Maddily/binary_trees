54c3efbe8c7abe4c009f3ab51ff04764f50e8f59
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-30 11:49:48 +0200

    Update function

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,11 +10,16 @@
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t nodes_left, nodes_right;
+
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left || tree->right)
-		return (1);
+	nodes_left = binary_tree_nodes(tree->left);
+	nodes_right = binary_tree_nodes(tree->right);
 
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	if (tree->left || tree->right)
+		return (1 + nodes_left + nodes_right);
+
+	return (0);
 }
