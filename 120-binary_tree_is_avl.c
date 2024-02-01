7f52ec1a1e10050cc8512f82f0536252c06fc14f
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-01 07:31:53 +0200

    Check for duplicate values

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,6 +1,45 @@
 #include "binary_trees.h"
 
 /**
+ * has_duplicates_helper - Recursively checks if a tree
+ * has duplicate values
+ *
+ * @tree: A pointer to the root of a tree
+ * @prev_value: A pointer to the previous value
+ *
+ * Return: 1 if there are duplicate values, 0 otherwise
+*/
+int has_duplicates_helper(const binary_tree_t *tree, int *prev_value)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (has_duplicates_helper(tree->left, prev_value))
+		return (1);
+
+	if (*prev_value == tree->n)
+		return (1);
+
+	*prev_value = tree->n;
+
+	return (has_duplicates_helper(tree->right, prev_value));
+}
+
+/**
+ * has_duplicates - Checks if a tree has duplicate values
+ *
+ * @tree: A pointer to the root of a tree
+ *
+ * Return: 1 if there are duplicate values, 0 otherwise
+*/
+int has_duplicates(const binary_tree_t *tree)
+{
+	int prev_value = 0;
+
+	return (has_duplicates_helper(tree, &prev_value));
+}
+
+/**
  * is_bst_helper - Checks if a binary tree is a binary search tree
  *
  * @tree: A pointer to the root of a binary tree
@@ -30,7 +69,7 @@ int is_bst_helper(const binary_tree_t *tree, int left_boundary,
 */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree == NULL || has_duplicates(tree))
 		return (0);
 
 	return (is_bst_helper(tree, INT_MIN, INT_MAX));
